77e6242a6685ae7d0f2b655a6efd78ba15b9c020
Sujan Bhusal <sujanbhusal@Sujans-MacBook-Air.local>
2026-01-18 21:52:10 -0500

    fixed the phantom space

--- app/components/work-section/Works.tsx
+++ app/components/work-section/Works.tsx
@@ -74,7 +74,7 @@ export default function Works() {
         "sagemaker",
       ],
       img: "https://images.unsplash.com/photo-1558494949-ef010cbdcc31?w=800&h=600&fit=crop",
-    },
+    }
   
   ];
 
